7478b0005bf68c9483aaf291a420c90e95d76553
tiwi7412 <tiwi7412@colorado.edu>
2022-09-21 15:02:08 -0600

    added specs

--- CSCI3302_Lab3/CSCI3302_Lab3/controllers/lab3/lab3.py
+++ CSCI3302_Lab3/CSCI3302_Lab3/controllers/lab3/lab3.py
@@ -6,8 +6,8 @@ from controller import Robot, Motor
 import math
 
 # TODO: Fill out with correct values from Robot Spec Sheet (or inspect PROTO definition for the robot)
-MAX_SPEED = 0.0001 # [rad/s]
-MAX_SPEED_MS = 0.0001 # [m/s]
+MAX_SPEED = 2.84 # [rad/s]
+MAX_SPEED_MS = .22 # [m/s]
 AXLE_LENGTH = 0.0001 # [m]
 
 
