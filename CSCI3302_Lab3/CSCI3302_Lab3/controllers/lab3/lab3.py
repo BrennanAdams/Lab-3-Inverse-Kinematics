d669e8e1bead314c5a35d29222cfe20c9080322b
tiwi7412 <tiwi7412@colorado.edu>
2022-09-21 15:19:29 -0600

    Added inverse kinematic equations

--- CSCI3302_Lab3/CSCI3302_Lab3/controllers/lab3/lab3.py
+++ CSCI3302_Lab3/CSCI3302_Lab3/controllers/lab3/lab3.py
@@ -8,7 +8,7 @@ import math
 # TODO: Fill out with correct values from Robot Spec Sheet (or inspect PROTO definition for the robot)
 MAX_SPEED = 2.84 # [rad/s]
 MAX_SPEED_MS = .22 # [m/s]
-AXLE_LENGTH = 0.0001 # [m]
+AXLE_LENGTH = .00160 # [m]
 
 
 
@@ -58,7 +58,8 @@ while robot.step(timestep) != -1:
     
     # STEP 1: Inverse Kinematics Equations (vL and vR as a function dX and dTheta)
     # Note that vL and vR in code is phi_l and phi_r on the slides/lecture
-    
+    vL = (dX - (dTheta/2))/(MAX_SPEED_MS/MAX_SPEED)
+    vR = (dX - (dTheta/2))/(MAX_SPEED_MS/MAX_SPEED)
     pass
     
     # STEP 2.3: Proportional velocities
